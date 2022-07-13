e005b849968b4c00c8befe21539b253451c70fe5
green <green>
2022-07-13 14:29:35 +0200

    Game working

--- sketch.js
+++ sketch.js
@@ -8,6 +8,110 @@ ml5 Example
 This example uses a callback pattern to create the classifier
 === */
 
+class Choice{
+  static Stein = new Choice("Stein")
+  static Schere = new Choice("Schere")
+  static Papier = new Choice("Papier")
+  static choices = [Choice.Stein, Choice.Papier, Choice.Schere]
+
+  //  Determines who wins the game Player 1 or 2
+  //  R P S 2
+  //R 0 2 1
+  //P 1 0 2
+  //S 2 1 0
+  //1
+  static winnerTable = [
+    [ 0, 2, 1],
+    [ 1, 0, 2],
+    [ 2, 1, 0],
+  ]
+
+  constructor(name){
+    this.choice = name
+  }
+
+  static fromString(label){
+    switch (label.toLowerCase()) {
+      case "schere", "scissors":
+        return Choice.Schere;
+      case "stein", "stone":
+        return Choice.Stein
+      case "papier", "paper":
+        return Choice.Papier
+      default:
+        throw `Cant match string to choice ${label.toLowerCase()}`
+    }
+  }
+
+  static getRandom(){
+    let a = int(Math.random() * 3)
+    return this.choices[a]
+  }
+
+  static getWinner(one, two){
+    if (one === undefined || two === undefined){
+      throw "Player one or two are undefined"
+      return "Error occured"
+    }
+    return this.winnerTable[this.choices.indexOf(one)][this.choices.indexOf(two)]
+  }
+  
+  toString(){
+    return this.choice;
+  }
+}
+
+class Player {
+  constructor(name){
+    this.name = name,
+    this.choice
+  }
+}
+
+class Game {
+
+  constructor(){
+    if (Game._instance){
+      return Game._instance;
+    }
+    Game._instance = this;
+    this.one = new Player("Spieler")
+    this.two = new Player("Computer")
+  }
+
+  calcComputerMove(){
+    this.two.choice = Choice.getRandom();
+  }
+
+  setPlayerMove(move){
+    this.one.choice = move
+  }
+
+  //returns int for player id 
+  getWinner(){
+    return Choice.getWinner(this.one.choice, this.two.choice)
+  }
+
+  //PlayerID to Message
+  getWinnerMessage(){
+    let winner = this.getWinner()
+
+    switch (winner) {
+      case 0:
+        return `Unentschieden mit\n${this.one.choice}`
+      
+      case 1: 
+        return `Spieler 1 gewinnt\n${this.one.choice} gegen ${this.two.choice}`
+
+      case 2:
+        return `Spieler 2 gewinnt\n${this.two.choice} gegen ${this.one.choice}`
+
+      default:
+        return "Fehler, bitte gucke in die Logs"
+    }
+  }
+}
+
 // Enum classame states
 class Stages {
   static None = new Stages("None");
@@ -27,7 +131,7 @@ class Stages {
 
 const STAGE = {
   _stage : Stages.None,
-  set current(stage){stageChanges(stage); this._stage = stage},
+  set current(stage) { this._stage = stage; stageChanges(stage, game);},
   get current() {return this._stage}
 }
 
@@ -54,9 +158,9 @@ function countToThree(callback) {
 //    prev = STAGE
 //  }
 //}
-
+// You could make this "case hell" more prettie with class patterns  
 // Like draw_stage but gets only executed when state changes
-function stageChanges(stage){
+function stageChanges(stage, game){
   switch (stage) {
     case Stages.Start:
       setTimeout(() => {
@@ -71,15 +175,16 @@ function stageChanges(stage){
       break;
     case Stages.Result:
       classifyVideo()
+      game.one.choice = Choice.fromString(resultss[0]["label"])
+      game.calcComputerMove()
       setTimeout(() => {
         STAGE.current = Stages.End
       }, 5000);
       break;
 
     case Stages.End:
-
       setTimeout(() => {
-        STAGE.current = Stages.Start
+        STAGE.current = Stages.Choosing
       }, 5000);
       break;
 
@@ -90,7 +195,7 @@ function stageChanges(stage){
 }
 
 // Function gets executed every frame
-function draw_stage(stage) {
+function draw_stage(stage, game) {
   fill(0)
   textSize(50)
   switch (stage) {
@@ -103,6 +208,7 @@ function draw_stage(stage) {
       text("Starting Game!", ratio * VIDEOHEIGHT / 2, VIDEOHEIGHT / 2)
       break;
     case Stages.Choosing:
+      background(255)
       image(video, 0, 0, ratio * VIDEOHEIGHT, VIDEOHEIGHT)
       textAlign(CENTER)
       fill(0)
@@ -112,7 +218,7 @@ function draw_stage(stage) {
       background(255)
       textAlign(CENTER)
       fill(0)
-      text("Computing", ratio * VIDEOHEIGHT / 2, VIDEOHEIGHT / 2)
+      text(game.getWinnerMessage(), ratio * VIDEOHEIGHT / 2, VIDEOHEIGHT / 2)
       break;
     default:
 
@@ -146,6 +252,8 @@ let ratio
 let classifier
 let video
 
+let game
+
 
 let resultss = {
   0: { label: "", confidence: 0 },
@@ -157,8 +265,9 @@ function setup() {
   // Create a camera input
   video = createCapture(VIDEO);
   video.hide();
-  console.log("I'm guessing a Video Ratio of " + video.width / video.height + "Its probably the wrong ratio however this is the only way to determine the video input")
   ratio = video.width / video.height
+
+  game = new Game();
 
   var canvas = createCanvas(VIDEOHEIGHT * ratio, VIDEOHEIGHT);
 
@@ -175,5 +284,5 @@ function draw() {
     ratio = video.width / video.height
     resizeCanvas(ratio * VIDEOHEIGHT, VIDEOHEIGHT)
   }
-  draw_stage(STAGE.current)
+  draw_stage(STAGE.current, game)
 }
